a3e1e8c928be17e48d344e1f5d06825dc5832d4d
Myv-hs <matthewcoyle@me.com>
2017-10-17 11:43:11 +0200

    TD2exo10

---
+++ TD2_2017/exo10.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <array>
+#include <string>
+
+int main() {
+	const int buff = 3;
+	bool mirroir = true;
+
+	std::array<int, buff> T1;
+	std::array<int, buff> T2;
+
+	std::cout << "Entrez les valeurs du premier tableau"<<std::endl;;
+	for(int i=0; i<buff; i++) {
+		std::cin >> T1[i];
+	}
+	std::cout << "Entrez les valeurs du deusieme tableau"<<std::endl;;
+	for(int i=0; i<buff; i++) {
+		std::cin >> T2[i];
+	}
+
+
+	for(int i=0; i<buff; i++){
+		if (T1[i] != T2[buff-i-1]) mirroir = false;
+	}
+
+	std::string rep = (mirroir) ? " sont " : " ne sont pas ";
+
+	std::cout << "Les deux tableaux" << rep << "mirroir" << std::endl;
+
+	return 0;
+}
