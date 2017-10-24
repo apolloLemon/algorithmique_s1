9be51fcd06efaca5858019b6d9766efb1ad4043e
Myv-hs <matthewcoyle@me.com>
2017-10-24 20:03:51 +0200

    exo13 added historic of wrong answers, and check it to avoid removing a point twice

--- TP2_2017/exo13.cpp
+++ TP2_2017/exo13.cpp
@@ -5,34 +5,44 @@ using namespace std;
 
 int main () {
 	string sol, myst;
-	char guess;
-	int wordsize, lives;
+	char guess, wguess[26];
+	int wordsize, lives, maxlives;
 
 	cin >> sol;
 	system("clear");
 
 	wordsize = sol.size();
-	lives = 3+(wordsize/4);
+	maxlives = 1+wordsize+(wordsize/4);
+	lives = maxlives;
 	myst = sol;
 	for(int i=0; i<wordsize;i++){
 		myst[i]='_';
 	}
 
 	do{
-		bool goodGuess = false;
+		bool looselife = true;
 		cout << myst << " (lives="<<lives<<")"<< endl;
 		cin >> guess;
+		for(int i=0; i<maxlives; i++){
+			if(guess==wguess[i]) {
+				looselife=false;
+			}
+		}
+
 		for(int i=0; i<wordsize;i++){
 			if(tolower(sol[i])==tolower(guess)) {
 				myst[i]=sol[i];
-				goodGuess = true;
+				looselife = false;
 			}
 		}
 		if(sol==myst) {
 			cout << sol<<"\nYou win" << endl;
 			return 0;
 		}
-		if(!goodGuess) lives--;
+		if(looselife) {
+			lives--;
+			wguess[(lives-maxlives)*-1]=guess;
+		}
 	} while (lives>=0);
 	cout << "You loose" << endl;
 
