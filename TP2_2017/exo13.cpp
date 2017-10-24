5732b8f082a699adf571c30f5ccfb820eeee8eb8
Myv-hs <matthewcoyle@me.com>
2017-10-24 14:08:11 +0200

    exo13

---
+++ TP2_2017/exo13.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+	string sol, myst;
+	char guess;
+	int wordsize, lives;
+
+	cin >> sol;
+
+	wordsize = sol.length();
+	lives = 3+(wordsize/4);
+	myst = sol;
+	for(int i=0; i<wordsize;i++){
+		myst[i]='_';
+	}
+
+	do{
+		bool goodGuess = false;
+		cout << myst << " (lives="<<lives<<")"<< endl;
+		cin >> guess;
+		for(int i=0; i<wordsize;i++){
+			if(tolower(sol[i])==tolower(guess)) {
+				myst[i]=sol[i];
+				goodGuess = true;
+			}
+		}
+		if(sol==myst) {
+			cout << "You win" << endl;
+			return 0;
+		}
+		if(!goodGuess) lives--;
+	} while (lives>=0);
+	cout << "You loose" << endl;
+
+	return 0;
+}
