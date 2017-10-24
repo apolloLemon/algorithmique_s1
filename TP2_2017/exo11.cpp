2970942427140913b66e29943b22f6d0323ec0bc
Myv-hs <matthewcoyle@me.com>
2017-10-24 21:58:23 +0200

    exo11minify

--- TP2_2017/exo11.cpp
+++ TP2_2017/exo11.cpp
@@ -6,19 +6,15 @@ using namespace std;
 int main () {
 	string usrin[2];
 	int letters[26]={0};
-	for (int i=0;i<2;i++){
-		getline(cin,usrin[i]);
-	}
+	for (int i=0;i<2;i++) getline(cin,usrin[i]);
 
 	for(int i=0;i<2;i++){
 		for(int j=0;j<usrin[i].size();j++){
 			unsigned int usrinI = tolower(usrin[i][j])-97;
-			if(usrinI<26){
-				letters[usrinI]++;
-			}
+			if(usrinI<26) letters[usrinI]++;
 		}
 	}
-	
+
 	for(int i=0;i<26;i++){
 		if(letters[i]%2!=0) {
 			cout << "not annagram"<<endl;
