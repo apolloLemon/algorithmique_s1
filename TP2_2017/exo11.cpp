d090a0beb6871c685ee962b8cf8ebd112beab3f1
Myv-hs <matthewcoyle@me.com>
2017-10-24 21:51:44 +0200

    exo11

---
+++ TP2_2017/exo11.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+	string usrin[2];
+	int letters[26]={0};
+	for (int i=0;i<2;i++){
+		getline(cin,usrin[i]);
+	}
+
+	for(int i=0;i<2;i++){
+		for(int j=0;j<usrin[i].size();j++){
+			unsigned int usrinI = tolower(usrin[i][j])-97;
+			if(usrinI<26){
+				letters[usrinI]++;
+			}
+		}
+	}
+	
+	for(int i=0;i<26;i++){
+		if(letters[i]%2!=0) {
+			cout << "not annagram"<<endl;
+			return 0;
+		}
+	}
+	cout << "annagram"<<endl;
+	return 0;
+}
