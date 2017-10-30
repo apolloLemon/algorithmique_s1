6b25ebdbe6f02679a85fe05e3609acd8b8a0aeab
Myv-hs <matthewcoyle@me.com>
2017-10-30 13:17:22 +0100

    fix and optimise

--- TP3_2017/exo03.cpp
+++ TP3_2017/exo03.cpp
@@ -2,26 +2,6 @@
 #include <string>
 using namespace std;
 
-void copy () {
-	string line1 = askforline("line to copy from");
-	unsigned int from=-1, size=-1;
-	unsigned linesize = line1.size();
-	while(from>linesize) from = askforint("From Position");
-	while(size>linesize-from+1) size = askforint("Number of characters");
-	string line2 = getcopy(line1,from,size);
-	cout<<line2<<endl;
-}
-
-string getcopy (string instr,int pos,int size) {
-	string outstring = "";
-	int start = pos-1;
-	int end = pos+size-1;
-	for(int i=start;i<end;i++){
-		outstring += instr[i];
-	}
-	return outstring;
-}
-
 string askforline (int linenum) {
 	string usrin = "";
 	cout << "Please write line "<<linenum<<":\n>> ";
@@ -51,17 +31,33 @@ int choosemode () {
 	return 0;
 }
 
+string getcopy (string instr,int pos,int size) {
+	string outstring = "";
+	int start = pos-1;
+	int end = pos+size-1;
+	for(int i=start;i<end;i++) outstring += instr[i];
+	return outstring;
+}
+
+void copy () {
+	string line1 = askforline("line to copy from");
+	unsigned int from=-1, size=-1;
+	unsigned linesize = line1.size();
+	while(from>linesize) from = askforint("From Position");
+	while(size>linesize-from+1) size = askforint("Number of characters");
+	string line2 = getcopy(line1,from,size);
+	cout<<line2<<endl;
+}
+
 int main () {
 	int mode=0;
-	while(!mode){
-		mode = choosemode();
-	}
+	while(!mode) mode = choosemode();
 	switch (mode) {
 		case 1:
 			copy();
 			break;
 		default:
-			cout << "som't wrong..";
+			cout << "Only 1 is availiable"<<endl;
 			break;
 	}	
 
