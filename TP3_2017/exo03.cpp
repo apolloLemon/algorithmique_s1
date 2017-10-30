f321f01b099077d634657920afb1fa5906e3202f
Myv-hs <matthewcoyle@me.com>
2017-10-30 14:31:16 +0100

    TP3exo3 del and add

--- TP3_2017/exo03.cpp
+++ TP3_2017/exo03.cpp
@@ -2,14 +2,14 @@
 #include <string>
 using namespace std;
 
-string askforline (int linenum) {
+string askforstring (int linenum) {
 	string usrin = "";
 	cout << "Please write line "<<linenum<<":\n>> ";
-	cin.ignore(); getline(cin, usrin);
+	getline(cin, usrin);
 	return usrin;
 }
 
-string askforline (string title) {
+string askforstring (string title) {
 	string usrin;
 	cout << "Please write "<<title<<":\n>> ";
 	cin.ignore(); getline(cin,usrin);
@@ -40,13 +40,55 @@ string getcopy (string instr,int pos,int size) {
 }
 
 void copy () {
-	string line1 = askforline("line to copy from");
+	string line1 = askforstring("line to copy from");
 	unsigned int from=-1, size=-1;
 	unsigned linesize = line1.size();
-	while(from>linesize) from = (unsigned int)askforint("From Position");
+	while(from>linesize) from = (unsigned int)askforint("From position");
 	while(size>linesize-from+1) size = (unsigned int)askforint("Number of characters");
 	string line2 = getcopy(line1,from,size);
 	cout<<line2<<endl;
+}
+
+string getdel (string instr, int pos, int size) {
+	string outstring = "";
+	int start = pos-1;
+	int end = pos+size-1;
+	for (int i=0; i<instr.size();i++){
+		if (i<start && i<end) outstring += instr[i];
+	}
+	return outstring;
+}
+
+void del () {
+	string line1 = askforstring("line to delete from");
+	unsigned int from=-1, size=-1;
+	unsigned linesize = line1.size();
+	while(from>linesize) from = (unsigned int)askforint("From position");
+	while(size>linesize-from+1) size = (unsigned int)askforint("Number of characters");
+	string line2 = getdel(line1,from,size);
+	cout<<line2<<endl;
+}
+
+string getaddstrings (string instr1, string instr2, int pos) {
+	string outstring = "";
+	if(pos==instr1.size()+1){
+		outstring = instr1+instr2;
+		return outstring;
+	}
+	for(int i=0;i<instr1.size();i++){
+		outstring += instr1[i];
+		if(i==pos-2) outstring +=instr2;
+	}
+	return outstring;
+}
+
+void addstrings () {
+	string line1 = askforstring("line 1 in which we'll add 2");
+	string line2 = askforstring(2);
+	unsigned int from=-1;
+	while(from>line1.size()+1) from = (unsigned int)askforint("Add line 2 to line 1 at");
+	string line3 = getaddstrings(line1,line2,from);
+	cout<<line3<<endl;
 }
 
 int main () {
@@ -55,6 +97,12 @@ int main () {
 	switch (mode) {
 		case 1:
 			copy();
+			break;
+		case 2:
+			del();
+			break;
+		case 3:
+			addstrings();
 			break;
 		default:
 			cout << "Only 1 is availiable"<<endl;
