cad948d416109d834d131f812b352f335b80ecf2
Myv-hs <matthewcoyle@me.com>
2017-11-06 11:06:19 +0100

    nb_jours_dans_mois method overload pour juste prendre le mois, ou prendre un string pour le mois

--- TP3_2017/exo04.cpp
+++ TP3_2017/exo04.cpp
@@ -7,7 +7,8 @@ bool est_bissextile (int y) {
 		return 1;
 	} else if (date%4==0 && date%100!=0){
 		return 1;
-	} else return 0;
+	}
+	return 0;
 }
 
 int nb_jours_dans_mois (int m, int y) {
@@ -17,7 +18,26 @@ int nb_jours_dans_mois (int m, int y) {
 		return 31;
 	} else if (m<=8 && m%2==0){
 		return 31;
-	}else return 30;
+	}
+	return 30;
+}
+
+int nb_jours_dans_mois (int m) {
+	if (m==2) {
+		cout << "Fevrier a besoin de l'annee\n";
+		return 0;
+	}
+	return nb_jours_dans_mois(m, 0);
+}
+
+int nb_jours_dans_mois (string m, int y) {
+	string mois[12] = {"janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"};
+	m = tolower(m);
+	for(int i=0;i<12;i++){
+		if(m==mois[i]) return nb_jours_dans_mois(i+1, y);
+	}
+	cout << "erreur dans la saisie du mois (n'utilisez pas d'accents)\nValeur de 30 jours utilise\n";
+	return 30;
 }
 
 int main () {
