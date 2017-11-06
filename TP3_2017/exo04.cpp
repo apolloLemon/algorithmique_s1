9e3eea9ed052ea37d40d8faa24da3e5ec14758fd
Myv-hs <matthewcoyle@me.com>
2017-11-06 11:20:59 +0100

    fix for est_bissextile wrong variable + fix for nb_jours_dans_mois(int m,int y) wrong function call

--- TP3_2017/exo04.cpp
+++ TP3_2017/exo04.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 bool est_bissextile (int y) {
-	if(date%400==0){
+	if(y%400==0){
 		return 1;
-	} else if (date%4==0 && date%100!=0){
+	} else if (y%4==0 && y%100!=0){
 		return 1;
 	}
 	return 0;
@@ -13,7 +13,7 @@ bool est_bissextile (int y) {
 
 int nb_jours_dans_mois (int m, int y) {
 	if (m==2) {
-		return 28+bisex(y);
+		return 28+est_bissextile(y);
 	} else if (m<8 && m%2!=0) {
 		return 31;
 	} else if (m<=8 && m%2==0){
@@ -30,15 +30,15 @@ int nb_jours_dans_mois (int m) {
 	return nb_jours_dans_mois(m, 0);
 }
 
-int nb_jours_dans_mois (string m, int y) {
+/*int nb_jours_dans_mois (string moisSaisi, int y) {
 	string mois[12] = {"janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"};
-	m = tolower(m);
+	moisSaisi = tolower(moisSaisi);
 	for(int i=0;i<12;i++){
-		if(m==mois[i]) return nb_jours_dans_mois(i+1, y);
+		if(moisSaisi==mois[i]) return nb_jours_dans_mois(i+1, y);
 	}
 	cout << "erreur dans la saisie du mois (n'utilisez pas d'accents)\nValeur de 30 jours utilise\n";
 	return 30;
-}
+}*/
 
 int main () {
 
