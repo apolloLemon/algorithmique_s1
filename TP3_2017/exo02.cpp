4c616bc9e90cc16ff2e1cb84793db083bede09bc
Myv-hs <matthewcoyle@me.com>
2017-11-09 19:50:22 +0100

    exo02

---
+++ TP3_2017/exo02.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int minimale () {
+
+}
+
+int maximale () {
+
+}
+
+int main () {
+	int buffSize = 512;
+	using TabPrecipitations = array<int,buffSize>;
+
+	TabPrecipitations Angers, Nantes, Rennes;
+	char 
+
+	return 0;
+}
