af9fb506bd9a3ffed4d7e8b10ecfc398242bb6c4
Myv-hs <matthewcoyle@me.com>
2017-11-13 02:29:48 +0100

    TP3exo02=75% + GetInt +Overload + min + max + minimale + maximale + main: gather Arrays, out Angers

--- TP3_2017/exo02.cpp
+++ TP3_2017/exo02.cpp
@@ -1,22 +1,81 @@
 #include <iostream>
 #include <array>
+#include <string>
 
 using namespace std;
+const unsigned int buffSize = 512;	
+using TabPrecipitations = array<int,buffSize>;
 
-int minimale () {
-
+int Getint (string name){
+	int usrin;
+	cout << "Entrez le nombre de valeurs a rentre pour:\n"<<name<<" :: ";
+	cin >> usrin;
+	return usrin;
 }
 
-int maximale () {
+int Getint (int n){
+	int usrin;
+	cout << "Quantite de precipitations pour le jour no "<<n<<" :: ";
+	cin >> usrin;
+	return usrin;	
+}
 
+int min (int a, int b){
+	if(a<=b) return a;
+	return b;
+}
+
+int max (int a, int b){
+	if(a>=b) return a;
+	return b;
+}
+
+int minimale (TabPrecipitations in, int size) {
+	int Cmin=in[0];
+	for(int i=1;i<size;i++) Cmin=min(Cmin,in[i]);
+	return Cmin;
+}
+
+int maximale (TabPrecipitations in, int size) {
+	int Cmax=in[0];
+	for(int i=1;i<size;i++) Cmax=max(Cmax,in[i]);
+	return Cmax;
 }
 
 int main () {
-	int buffSize = 512;
-	using TabPrecipitations = array<int,buffSize>;
-
 	TabPrecipitations Angers, Nantes, Rennes;
-	char 
+	unsigned int As, Ns, Rs;
+	char town;
+	do {
+		cout << "Pour quel ville voulez vous entrez les precipitations?\nA=Angers, N=Nantes, R=Rennes, Z=fin :: ";
+		cin >> town;
+		town = tolower(town);
+		switch(town){
+			case 97:
+				As = Getint("Angers");
+				for(int i=0; i<As;i++){
+					Angers[i]=Getint(i+1);
+				}
+				cout<<"Max Angers = "<<maximale(Angers, As);
+				cout<<"Min Angers = "<<minimale(Angers, As);
+				break;
+			case 110:
+				Ns = Getint("Nantes");
+				for(int i=0; i<Ns;i++){
+					Nantes[i]=Getint(i+1);
+				}
+				break;
+			case 114:
+				Rs = Getint("Rennes");
+				for(int i=0; i<Rs;i++){
+					Rennes[i]=Getint(i+1);
+				}
+				break;
+			default:
+				break;
+		}
+	} while (town-122);
 
+	cout << endl;
 	return 0;
 }
