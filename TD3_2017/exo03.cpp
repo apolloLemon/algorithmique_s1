d78b1895d21ecdb4d0696a6c1678a466bc11269b
Myv-hs <matthewcoyle@me.com>
2017-11-19 22:18:18 +0100

    TD3exo03 c) d)

--- TD3_2017/exo03.cpp
+++ TD3_2017/exo03.cpp
@@ -3,15 +3,37 @@
 
 using namespace std;
 
-const unsigned int buffSize = 512; //a
-using TabAges = array<int, buffSize>;
+const unsigned int maxage = 120; //a
+using TabAges = array<int, maxage>;
 TabAges ages;
 
 void init () { //b
 	ages = {0};
 }
 
+void saisie () { //c
+	unsigned int n,x;
+	cout << "combien d\'individues a saisir l\'age: ";
+	cin >> n;
+	for(int i=0;i<n;i++) {
+		cin >>x;
+		if(x<maxage) ages[x]++;
+	}
+}
+
+float moyenne (TabAges a) { //d
+	int individues, age_total;
+	for(int i=0;i<maxage;i++) {
+		age_total += i*a[i];
+		individues += a[i];
+	}
+	//cout << age_total << " " << individues; //++ this line, --(float)for individues has same result
+	return (float)age_total/(float)individues;
+}
+
 int main () {
-	
+	init();
+	saisie();
+	cout << "age moyen = " << moyenne(ages)<<endl;
 	return 0;
 }
