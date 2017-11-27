398826001da94fd7dfbd248ea206ced46948528f
Myv-hs <matthewcoyle@me.com>
2017-11-27 12:04:16 +0100

    TP3exo4 ++string nextday (d,m,y) ++string datestring (d,m,y)

--- TP3_2017/exo04.cpp
+++ TP3_2017/exo04.cpp
@@ -26,7 +26,7 @@ int nb_jours_dans_mois (string m, int y) {
 	string mois[12] = {"janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"};
 	string moisSaisi = "";
 	for(int i=0;i<m.size();i++) moisSaisi+=tolower(m[i]);
-	for(int i=0;i<12();i++){
+	for(int i=0;i<12;i++){
 		if(moisSaisi==mois[i]) return nb_jours_dans_mois(i+1, y);
 	}
 	cout << "erreur dans la saisie du mois (n'utilisez pas d'accents)\nValeur de 30 jours utilise\n";
@@ -43,7 +43,26 @@ bool date_valide (unsigned int d, unsigned int m, int y) {
 	return 0;
 }
 
+string datestring (int d, int m, int y){
+	string s=to_string(d);
+	s+='/';
+	s+=to_string(m);
+	s+='/';
+	s+=to_string(y);
+	return s;
+}
+
+string nextday (int d, int m, int y) {
+	string outString = "";
+	if (!date_valide(d,m,y)) return "Date non valide";
+	if (date_valide(d+1,m,y)) return datestring(d+1,m,y);
+	if (date_valide(1,m+1,y)) datestring(1,m+1,y);
+	return datestring(1,1,y+1);
+}
+
 int main () {
-	
+	int d, m, y;
+	cin >> d >> m >> y;
+	cout << nextday(d,m,y) << endl;
 	return 0;
 }
