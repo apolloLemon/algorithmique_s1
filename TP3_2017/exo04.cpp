118a032654cccfd14af8520ac2b62447ec8eb029
Myv-hs <matthewcoyle@me.com>
2017-11-30 09:52:58 +0100

    TP3exo4 split date valide en jour/mois valide -- nb_jours_dans_mois (string) fix nextday

--- TP3_2017/exo04.cpp
+++ TP3_2017/exo04.cpp
@@ -16,20 +16,9 @@ int nb_jours_dans_mois (unsigned int m, int y) {
 		return 28+est_bissextile(y);
 	} else if (m<8 && m%2!=0) {
 		return 31;
-	} else if (m<=8 && m%2==0){
+	} else if (m>=8 && m%2==0){
 		return 31;
 	}
-	return 30;
-}
-
-int nb_jours_dans_mois (string m, int y) {
-	string mois[12] = {"janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"};
-	string moisSaisi = "";
-	for(int i=0;i<m.size();i++) moisSaisi+=tolower(m[i]);
-	for(int i=0;i<12;i++){
-		if(moisSaisi==mois[i]) return nb_jours_dans_mois(i+1, y);
-	}
-	cout << "erreur dans la saisie du mois (n'utilisez pas d'accents)\nValeur de 30 jours utilise\n";
 	return 30;
 }
 
@@ -38,9 +27,17 @@ int nb_jours_dans_mois (unsigned int m) {
 	return nb_jours_dans_mois(m, 1);
 }
 
-bool date_valide (unsigned int d, unsigned int m, int y) {
+bool jour_valide (unsigned int d, unsigned int m, int y) {
 	if (d<=nb_jours_dans_mois(m,y)) return 1;
 	return 0;
+}
+bool mois_valide (unsigned int m) {
+	if(m<13) return 1;
+	return 0;
+}
+
+bool date_valide (unsigned int d, unsigned int m, int y) {
+	return jour_valide(d,m,y)*mois_valide(m);
 }
 
 string datestring (int d, int m, int y){
@@ -56,7 +53,7 @@ string nextday (int d, int m, int y) {
 	string outString = "";
 	if (!date_valide(d,m,y)) return "Date non valide";
 	if (date_valide(d+1,m,y)) return datestring(d+1,m,y);
-	if (date_valide(1,m+1,y)) datestring(1,m+1,y);
+	if (date_valide(1,m+1,y)) return datestring(1,m+1,y);
 	return datestring(1,1,y+1);
 }
 
