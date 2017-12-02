bca0c2cba78dded2579d38065eaebe83fb1195d5
Myv-hs <matthewcoyle@me.com>
2017-12-02 21:56:54 +0100

    J'ai fait une version comme ce qu'on aurrais rendu en CC et une version qui peux passer le compilateur pour etre teste

--- CC_30112017/cExo1.cpp
+++ CC_30112017/cExo1.cpp
@@ -1,3 +1,11 @@
+//je met l'include pour pouvoir tester dans le compilateur
+#include <array>
+#include <iostream>
+
+//definit dans l'ennonce
+const int Taille = 10;
+using tablo = std::array<int, Taille>;
+
 //1
 int maximale (tablo T) {
 	int max = T[0];
@@ -33,4 +41,16 @@ bool Plus_grand_3 (tablo T1, tablo T2) {
 		if(T1[i]>=T2[i]) a++;
 	}
 	return (a==Taille);
+}
+
+/*
+*/
+int main () {
+	tablo T1, T2;
+	std::cout << "Entrez les valeurs de T1 une a une, ou separee d'espaces (10 valeurs)\n";
+	for(int i=0;i<Taille;i++) std::cin >> T1[i];
+	std::cout << "Entrez les valeurs de T2 une a une, ou separee d'espaces (10 valeurs)\n";
+	for(int i=0;i<Taille;i++) std::cin >> T2[i];
+	int foisqueT1estplusgrandqueT2 = Plus_grand_1(T1,T2)+Plus_grand_2(T1,T2)+Plus_grand_3(T1,T2);
+	std::cout << foisqueT1estplusgrandqueT2 << std::endl;
 }
