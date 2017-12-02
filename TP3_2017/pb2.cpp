eb432ff79e9a2d90d571f40eccc65700ae5c31c6
Myv-hs <matthewcoyle@me.com>
2017-12-02 19:27:22 +0100

    init fix, ++ void afficher

--- TP3_2017/pb2.cpp
+++ TP3_2017/pb2.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
 #include <array>
-
 using namespace std;
-
 const unsigned int buffSize=512;
 using platojeu = array<char, buffSize>;
-platojeu jeu;
-unsigned int gamesize;
 
-void init () {
-	cin >> gamesize;
-	if(gamesize>20) gamesize=20;
-	jeu = {'X'};
-	for(int i=0;i<gamesize;i++){
-		jeu[i]='B';
+bool gagne (platojeu a, int b) {
+
+}
+
+void init (platojeu &a, int b) {
+	a[b] = {'X'};
+	for(int i=0;i<b;i++){
+		a[i]='B';
 	}
-	for(int i=gamesize*2;i>gamesize;i--){
+	for(int i=b*2;i>b;i--){
+		a[i]='N';
+	}
+}
 
+void afficher (platojeu a, int b) {
+	for(int i=0;i<b;i++) {
+		cout << a[i];
 	}
 }
 
 int main () {
+	platojeu jeu;
+	unsigned int sheepcount, gamesize;
+	cin >> sheepcount; 
+	if(sheepcount>20) sheepcount=20; gamesize = 2*sheepcount+1;
 
+	init(jeu, sheepcount);
+	afficher(jeu, gamesize);
+
+	cout << endl;
 	return 0;
 }
