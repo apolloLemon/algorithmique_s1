fda550b2207d53f0082582d3b9068a723a913713
Myv-hs <matthewcoyle@me.com>
2017-12-03 12:52:07 +0100

    exo2 1-7

---
+++ CC_30112017/Exo2.cpp
@@ -0,0 +1,54 @@
+//1
+const int maxtrax = 100;
+using tab_tracks = std::array<int,maxtrax>;
+
+//2
+int saisie (tab_tracks &a) {
+	int N;
+	cout << "Donner votre nombre de morceaux : ";
+	cin >> N;
+	for(int i=0;i<N;i++) {
+		cout << "Donner la duree en secondes : ";
+		cin >> a[i];
+	}
+	return N;
+}
+
+//3
+int duree_totale (tab_tracks a,int N){
+	int stot = 0;
+	for(int i=0;i<N;i++) stot += a[i];
+	return stot;
+}
+
+//4
+bool long_sup_5 (tab_tracks a, int M){
+	return (a[M]>5*60);
+}
+
+//5
+void aff_liste(tab_tracks a, int N, int Dmax) {
+	int stot =0;
+	for (int i=0;i<N;i++){
+		if(stot+a[i]>=Dmax) stot+=a[i];
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+//6
+using tab_titres = std::array<string,maxtrax>;
+
+//7
+int saisie (tab_tracks &a, tab_titres &b) {
+	int N;
+	cout << "Donner votre nombre de morceaux : ";
+	cin >> N;
+	for(int i=0;i<N;i++) {
+		cout << "Donner le titre d'un morceau : ";
+		cin >> b[i];
+		cout << "Donner sa duree en secondes : ";
+		cin >> a[i];
+	}
+	return N;
+}
