668b9866e81e18d29cd8b0355221c32acfb4e91b
Myv-hs <matthewcoyle@me.com>
2017-12-26 19:58:22 +0100

    sudoku exo3

--- CM_Sudoku/sudoku.cpp
+++ CM_Sudoku/sudoku.cpp
@@ -3,7 +3,7 @@
 
 //1.1
 const unsigned int gridSize = 9;
-using ligne = std::array<int, gridSize>;
+using ligne = std::array<unsigned int, gridSize>;
 using sudoku = std::array<ligne, gridSize>;
 
 //1.2
@@ -41,9 +41,42 @@ bool verif_col(sudoku a,int val,int col) {
 	return false;
 }
 
+//3.1
+void saisie(sudoku &a) {
+	bool saisieFini = false;
+	while(!saisieFini){
+		unsigned int val, x, y;
+		std::cout<<"Entrez valeur, coor x et coor y separees d'espaces\nvaleur_a_enter coord_x coord_y :: ";
+		std::cin>>val>>x>>y;
+		if(val>9 || val==0 || x >=9 || y >=9) {
+			std::cout<<"\nValeur, x ou y incorrect. ";
+			continue;
+		}
+		if(verif_ligne(a,val,y) || verif_col(a,val,x)) {
+			std::cout<<"\nLigne ou Colone deja occupe. ";
+			continue;
+		}
+		a[y][x]=val;
+		saisieFini = true;
+	}	 
+}
+
+//3.2
+bool fin (sudoku a){
+	for(int i=0;i<gridSize;i++){
+		for(int j=0;j<gridSize;j++){
+			if (a[i][j]==0) return false;
+		}
+	}
+	return true;
+}
+
 //Tester Main
 int main () {
 	sudoku S;
 	init(S);
-	affiche(S);
+	while(!fin(S)){
+		affiche(S);
+		saisie(S);
+	}
 }
