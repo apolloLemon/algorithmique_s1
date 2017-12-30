218790578b424e2370d3f5d534c5768e9d3f44e2
Myv-hs <matthewcoyle@me.com>
2017-12-30 12:22:17 +0100

    loto.cpp exo1

---
+++ CM_Loto/loto.cpp
@@ -0,0 +1,13 @@
+//Dans les rappels
+#include <iostream>
+#include <array>
+#include <ctime>
+#include <cstlib>
+
+
+//1
+const unsigned int ValMax = 49;
+const unsigned int Taille = 5;
+using grille = std::array<unsigned int, Taille>;
+
+//
