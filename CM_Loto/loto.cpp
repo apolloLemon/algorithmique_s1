6d505f778bf14c116d9dc08fedf654f1c9f7272d
Myv-hs <matthewcoyle@me.com>
2017-12-30 12:30:47 +0100

    loto.cpp exo2

--- CM_Loto/loto.cpp
+++ CM_Loto/loto.cpp
@@ -6,8 +6,24 @@
 
 
 //1
-const unsigned int ValMax = 49;
-const unsigned int Taille = 5;
+const int ValMax = 49;
+const int Taille = 5;
 using grille = std::array<unsigned int, Taille>;
 
-//
+//2
+void saisie (grille &a) {
+	for(int i=0;i<Taille;i++){
+		bool saisieCorrect = false;
+		while(!saisieCorrect){
+			int b;
+			std::cout <<"numero "<<i+1<<" : ";
+			std::cin >> b;
+			if(b>0 && b<=ValMax){
+				a[i]=b;
+				saisieCorrect=true;
+			} else {
+				std::cout<<"Incorrect, les numeros doivent etres compris entre 1 et "<<ValMax<<std::endl;
+			}
+		}
+	}
+}
